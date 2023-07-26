34b34ee552b2dca3bdb37de80d2591c147088d81
cuong <cuong16102002@gmail.com>
2023-07-26 20:43:23 +0700

    kanzen startstring -> lat long district

--- src/controller/test-geocoding-controller.ts
+++ src/controller/test-geocoding-controller.ts
@@ -1,51 +1,45 @@
 import axios from "axios";
-import { Request, Response } from "express";
-const address = "Chợ Ngọc Thuỵ";
-const apiKey = "AIzaSyD_aa2r6r8WRRVHUpO8IU18gUD4evNmXt0";
-const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-  address
-)}&key=${apiKey}`;
-console.log(url);
-export let lat = 0.0;
-export let long = 0.0;
-export let compoundCode = "";
-export let district = "";
-export function callGetUsersRoute(req: Request, res: Response) {
-  //const url = "http://localhost:8000/test-geocoding";
 
-  // Gửi yêu cầu GET đến route
-  axios
-    .get(url)
-    .then((response) => {
-      const data = response.data;
-      const results = data.results;
-      if (results.length > 0) {
-        const firstResult = results[0];
-        const geometry = firstResult.geometry;
-        const location = geometry.location;
-        //const plusCode = firstResult.plus_code;
-        compoundCode = firstResult.plus_code?.compound_code;
-        const firstSpaceIndex = compoundCode.indexOf(" ");
-        const modifiedCompoundCode =
-          compoundCode.slice(0, firstSpaceIndex) +
-          "," +
-          compoundCode.slice(firstSpaceIndex + 1);
-        const result = modifiedCompoundCode
-          .split(",")
-          .map((part) => part.trim());
-        //console.log(result);
-        district = result[1];
-        lat = location.lat;
-        long = location.lng;
+interface LocationIn4 {
+  lat: number;
+  long: number;
+  district: string;
+}
 
-        console.log(
-          `Latitude: ${lat}, Longitude: ${long}, district: ${district}`
-        );
-        res.status(200).json({ lat: lat, long: long, district: district });
-      }
-    })
-    .catch((error) => {
-      console.error("Error retrieving geocode:", error);
-      res.status(200).json({ message: "fail" });
-    });
+export async function convertIn4(address: string): Promise<LocationIn4> {
+  const ans: LocationIn4 = {
+    lat: 0.0,
+    long: 0.0,
+    district: "ha noi",
+  };
+  let compoundCode = "";
+  const apiKey = "AIzaSyD_aa2r6r8WRRVHUpO8IU18gUD4evNmXt0";
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+    address
+  )}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    const results = data.results;
+    if (results.length > 0) {
+      const firstResult = results[0];
+      const geometry = firstResult.geometry;
+      const location = geometry.location;
+      compoundCode = firstResult.plus_code?.compound_code;
+      const firstSpaceIndex = compoundCode.indexOf(" ");
+      const modifiedCompoundCode =
+        compoundCode.slice(0, firstSpaceIndex) +
+        "," +
+        compoundCode.slice(firstSpaceIndex + 1);
+      const result = modifiedCompoundCode.split(",").map((part) => part.trim());
+      ans.district = result[1];
+      ans.lat = location.lat;
+      ans.long = location.lng;
+    }
+    return ans;
+  } catch (error) {
+    console.error("Error retrieving geocode:", error);
+    throw error;
+  }
 }
--- src/controller/user-start-string-controller.ts
+++ src/controller/user-start-string-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import UserStartString from "../models/user-start-string";
+import { convertIn4 } from "./test-geocoding-controller";
 
 // use array for future has many user, one user has one id and one thread to solve the problem
 export let userStartStrings: UserStartString[] = [
@@ -8,8 +9,10 @@ export let userStartStrings: UserStartString[] = [
     startString: "",
   },
 ];
-
+let startString1 = "";
 export const getUserStartString = (req: Request, res: Response) => {
+  //let convert = convertIn4(startString);
+  //console.log(convert.lat);
   res.status(200).json({ userStartString: userStartStrings });
 };
 
@@ -24,6 +27,25 @@ export async function updateUserStartString(req: Request, res: Response) {
 
   if (index !== -1) {
     userStartStrings[index].startString = startString;
+    startString1 = startString;
+    startString1 = startString1.concat(" Hà Nội, Việt Nam");
+    //console.log(startString1);
+
+    try {
+      const convert = await convertIn4(startString1);
+      if (convert.district.toLowerCase().includes("district")) {
+        // Xoá xâu "District" khỏi "district" (không phân biệt chữ hoa/thường)
+        convert.district = convert.district.replace(/district/gi, "").trim();
+      }
+      console.log({
+        lat: convert.lat,
+        long: convert.long,
+        district: convert.district,
+      });
+    } catch (error) {
+      console.error("Error converting address to LocationIn4:", error);
+      // Handle error if needed
+    }
     res.status(200).json({
       message: "updated successfully",
       userStartString: userStartStrings,
