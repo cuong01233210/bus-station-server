374fd8323a77af0f5d15c7dd80a4c1bc4a6f2a9b
cuong <cuong16102002@gmail.com>
2023-12-29 11:40:14 +0700

    phân role staff user

--- src/models/user-login.ts
+++ src/models/user-login.ts
@@ -3,32 +3,23 @@ import { Db } from "mongodb";
 
 class LoginUser {
   id?: string;
-  name: string;
+  //name: string;
   email: string;
   password: string;
+  role: string; // 1: quan ly, 0: ng dung
 
-  constructor(name: string, email: string, password: string, id?: string) {
+  constructor(email: string, password: string, role: string, id?: string) {
     this.id = id;
-    this.name = name;
+    //this.name = name;
     this.email = email;
     this.password = password;
+    this.role = role;
   }
 
   async createUser() {
     const db: Db = LoginDbs.getDb();
-    // const documents1 = await db.collection("users").find().toArray();
-    // const login1 = documents1.map(
-    //   (doc) => new LoginUser(doc.name, doc.email, doc.password)
-    // );
-    // console.log(login1);
     delete this.id;
     const insertOneResult = await db.collection("users").insertOne({ ...this });
-    // const db2: Db = LoginDbs.getDb();
-    // const documents2 = await db.collection("users").find().toArray();
-    // const login2 = documents2.map(
-    //   (doc) => new LoginUser(doc.name, doc.email, doc.password)
-    // );
-    // console.log(login2);
     return insertOneResult.insertedId.toString();
   }
   static empty = new LoginUser("", "", "", "");
@@ -39,9 +30,9 @@ class LoginUser {
     console.log("user dang login", document);
     if (document != null) {
       return new LoginUser(
-        document.name,
         document.email,
         document.password,
+        document.role,
         document._id.toString()
       );
     } else return LoginUser.empty;
