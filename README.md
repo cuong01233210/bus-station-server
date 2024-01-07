3d97cc3d5e5cf5da45b4bc370cea5cd506ed8611
cuong <cuong16102002@gmail.com>
2024-01-07 16:26:22 +0700

    xong delete trạm 7/1/24

--- README.md
+++
@@ -1,1 +0,0 @@
-# bus-station-server
--- src/models/best-pair.ts
+++
@@ -1,8 +0,0 @@
-import MyNode from "./my-node";
-import MyPoint from "./my-point";
-interface BestPair {
-  point: MyPoint | null;
-  dist: number;
-}
-
-export default BestPair;
