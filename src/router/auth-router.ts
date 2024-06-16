d17d575ae1040f476ff6975f8f0355a11260dc48
cuong01233210 <cuong16102002@gmail.com>
2024-06-17 00:55:26 +0700

    xong create staff 17/6/24

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as authController from "../controller/customer's-controller/auth-controller";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 import authValidator from "../middleware/auth-validation";
 const authRouter = Router();
 
@@ -46,6 +46,22 @@ authRouter.get(
   "/get-staffs",
   authValidator,
   authController.getStaffsController
+);
+
+authRouter.post(
+  "/add-staff",
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Xin hãy nhập đúng định dạng email")
+      .normalizeEmail(),
+    body("phoneNumber")
+      .trim()
+      .isMobilePhone("vi-VN")
+      .withMessage("Xin hãy nhập đúng định dạng số điện thoại"),
+  ],
+  authController.addStaffController
 );
 
 export default authRouter;
